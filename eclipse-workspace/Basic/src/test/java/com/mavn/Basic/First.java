7fd5998d81268a195146d420943a42d0eb88e708
soujanya.pitla <soujanya.pitla@volantetech.com>
2021-06-05 21:08:25 +0530

    add

---
+++ eclipse-workspace/Basic/src/test/java/com/mavn/Basic/First.java
@@ -0,0 +1,13 @@
+package com.mavn.Basic;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("Hai...");
+		System.out.println("Hllo...");
+		System.out.println("Hllo...");
+
+
+	}
+
+}
